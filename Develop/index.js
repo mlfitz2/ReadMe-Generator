cbd33f8023bc890b020d5a02b4b5da9ff1fcaafe
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-02-16 20:49:55 -0800

    prompts are working correctly

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,104 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is this project called?'
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What was your motivation in building it?'
+        },
+        {
+            type: 'input',
+            name: 'problems',
+            message: 'What problems does it solve?'
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: 'What did you learn while building it?'
+        },
+        {
+            type: 'input',
+            name: 'standout',
+            message: 'What makes this project stand out?'
+        },
+        {
+            type: 'input',
+            name: 'techs',
+            message: 'What technologies were used in building it?'
+        },
+        {
+            type: 'input',
+            name: 'screenshot',
+            message: 'Enter a link to your screenshot.'
+        },
+    ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateFile = ({name, motivation, problems, learn, standout, techs, screenshot}) =>
+` 
+# ${name}
 
-// TODO: Create a function to initialize app
-function init() {}
+# Motivation in building ${name}:
+${motivation}
 
-// Function call to initialize app
+# Problems solved by ${name}:
+${problems}
+
+# Building ${name} taught me:
+${learn}
+
+# ${name} stands out because:
+${standout}
+
+# Technologies used:
+${techs}
+
+Screenshot:
+${screenshot}
+`;
+
+const init = () => {
+    questions()
+    .then((answers) => fs.writeFileSync('readmetest.md', generateFile(answers)))
+    .then(() => console.log('Your ReadMe file has been generated!'))
+    .catch((err) => console.error(err));
+}
+
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
---
+++ Develop/readmetest.md
@@ -0,0 +1,20 @@
+ 
+# PubCrawler
+
+# Motivation in building PubCrawler:
+To find great breweries nearby.
+
+# Problems solved by PubCrawler:
+Having to find breweries on your own, and calculating the best route for a pubcrawl on your own.
+
+# Building PubCrawler taught me:
+How to use server-side APIs, and how to work in a group on GitHub.
+
+# PubCrawler stands out because:
+It is excellent in every way.
+
+# Technologies used:
+HTML, CSS, JavaScript.
+
+Screenshot:
+www.pretend-this-is-a-screenshot.com
